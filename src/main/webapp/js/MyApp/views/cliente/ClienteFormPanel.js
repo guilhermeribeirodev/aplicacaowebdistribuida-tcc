a5b81e83574945618d5cc5f167381d59ac049eb8
guilherme_ribeiro <guilhermedoceu@gmail.com>
2013-11-29 17:42:00 -0200

    form

--- src/main/webapp/js/MyApp/views/cliente/ClienteFormPanel.js
+++ src/main/webapp/js/MyApp/views/cliente/ClienteFormPanel.js
@@ -39,8 +39,8 @@ Ext.define('MyApp.views.cliente.ClienteFormPanel', {
                         flex: 1
                     },
                     {
-                        fieldLabel: 'Sobrenome',
-                        name: 'sobrenome',
+                        fieldLabel: 'Documento',
+                        name: 'documento',
                         flex: 1
                     }
                 ]
@@ -55,129 +55,17 @@ Ext.define('MyApp.views.cliente.ClienteFormPanel', {
                 },
                 items: [
                     {
-                        fieldLabel: 'RG',
+                        fieldLabel: 'Idade',
                         afterLabelTextTpl: required,
-                        name: 'last',
-                        width: 200
-                    },
-                    {
-                        fieldLabel: 'Email',
-                        afterLabelTextTpl: required,
-                        name: 'email',
-                        vtype: 'email',
+                        name: 'idade',
                         flex: 1
+
                     }
-                ]
-            },
-            {
-                xtype: 'fieldcontainer',
-                layout: 'hbox',
-                defaultType: 'textfield',
-                fieldDefaults: {
-                    labelAlign: 'top',
-                    margins: '0 10 10 0'
-                },
-                items: [
-                    {
-                        xtype: 'datefield',
-                        fieldLabel: 'Data de nascimento',
-                        afterLabelTextTpl: required,
-                        name: 'nascimento',
-                        submitFormat: 'Y-m-d',
-                        format: 'd/m/Y',
-                        margins: '0 30 10 0',
-                        width: 150
-                    },
-                    {
-                        xtype: 'checkbox',
-                        fieldLabel: 'Mailing',
-                        allowBlank: false,
-                        name: 'mailing',
-                        width: 100
-                    },
-                    {
-                        xtype: 'checkbox',
-                        fieldLabel: 'Blacklisted',
-                        allowBlank: false,
-                        name: 'blacklisted',
-                        width: 100
-                    },
-                    {
-                        xtype: 'checkbox',
-                        fieldLabel: 'VIP',
-                        allowBlank: false,
-                        name: 'vip',
-                        width: 100
-                    },
-                    {
-                        xtype: 'checkbox',
-                        fieldLabel: 'Ativo',
-                        allowBlank: false,
-                        name: 'ativo',
-                        width: 100
-                    }
-                ]
-            },
-            {
-                xtype: 'tabpanel',
-                plain: true,
-                activeTab: 0,
-                height: 235,
-                defaults: {
-                    margins: '10 0',
-                    bodyPadding: 10
-                },
-                items: [
-                    {
-                        title: 'Contato',
-                        defaults: {
-                            width: 230
-                        },
-                        defaultType: 'textfield',
 
-                        items: [
-
-
-                            {
-                                fieldLabel: 'Endereço',
-                                name: 'endereco',
-                                width: 400,
-                                allowBlank: false
-                            },
-                            {
-                                fieldLabel: 'Telefone Celular',
-                                name: 'telefoneCel'
-                            },
-                            {
-                                fieldLabel: 'Telefone Residencial',
-                                name: 'telefoneRes'
-                            }
-                        ]
-                    },
-                    {
-                        title: 'Dispositivos',
-                        defaults: {
-                            width: 230
-                        },
-                        defaultType: 'textfield',
-
-                        items: [
-
-                        ]
-                    }
-                    /*{
-                        cls: 'x-plain',
-                        title: 'Biography',
-                        layout: 'fit',
-                        items: {
-                            xtype: 'htmleditor',
-                            name: 'bio2',
-                            fieldLabel: 'Biography'
-                        }
-                    }*/
                 ]
             }
-        ];
+        ]
+
 
     },
 
@@ -193,57 +81,24 @@ Ext.define('MyApp.views.cliente.ClienteFormPanel', {
                 var win = this.up('window')
 
 
-                if(formPanel.edicao){
 
-                    var cliente = MyApp.stores.ClienteStore.getById(form._record.data.id);
+                var cliente = Ext.create('MyApp.models.Cliente', form.getFieldValues());
 
-                    cliente.beginEdit();
+                cliente.save({
+                    success: function (cliente) {
+                        console.log(cliente)
 
-                    cliente.set(form.getValues());
-
-                    cliente.commit()
-
-                    // MyApp.stores.ItemStore.sync();
-                    cliente.save({
-                        success: function (item) {
-                            console.log(item)
-
-                            Ext.MessageBox.alert('Alterar cliente', 'Alterado com sucesso');
+                        Ext.MessageBox.alert('Adiconar cliente', 'Adicionado com sucesso');
 
 
-                            form.reset()
-                            win.removeAll();
-                            win.add(MyApp.views.cliente.ClienteGridPanel);
-                            MyApp.stores.ClienteStore.load();
+                        form.reset()
+                        win.removeAll();
+                        win.add(MyApp.views.cliente.ClienteGridPanel);
+                        MyApp.stores.ClienteStore.load();
 
 
-                        }
-                    });
-
-
-
-
-                } else{
-
-                    var cliente = Ext.create('MyApp.models.Cliente', form.getFieldValues());
-
-                    cliente.save({
-                        success: function (cliente) {
-                            console.log(cliente)
-
-                            Ext.MessageBox.alert('Adiconar cliente', 'Adicionado com sucesso');
-
-
-                            form.reset()
-                            win.removeAll();
-                            win.add(MyApp.views.cliente.ClienteGridPanel);
-                            MyApp.stores.ClienteStore.load();
-
-
-                        }
-                    });
-                }
-
+                    }
+                });
 
             }
         },
@@ -259,8 +114,6 @@ Ext.define('MyApp.views.cliente.ClienteFormPanel', {
                 win.removeAll();
                 win.add(MyApp.views.cliente.ClienteGridPanel);
                 MyApp.stores.ClienteStore.load();
-
-
 
 
             }
